f0d01367783ed2df0f3e9e26b92a64372bac304f
AMGsmert69 <amgsmart69@gmail.com>
2023-12-11 13:47:39 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+import Home from "./comp/Home"
+import Header from "./comp/Header"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import App from "./App";
+
+import global_en from "./translations/en/global.json";
+import global_es from "./translations/es/global.json";
+import global_ar from "./translations/ar/global.json";
+import i18next from "i18next";
+import { I18nextProvider } from "react-i18next";
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  resources: {
+    en: {
+      global: global_en,
+    },
+    es: {
+      global: global_es,
+    },
+    ar: {
+      global: global_ar,
+    },
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
